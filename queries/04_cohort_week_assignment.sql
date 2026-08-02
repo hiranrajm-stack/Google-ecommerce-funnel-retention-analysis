379f0a248bc8258f16b5772b4626e00ece469b6c
hiranrajm-stack <mhiranraj@gmail.com>
2026-08-02 12:10:42 +0530

    Update 04_cohort_week_assignment.sql

--- queries/04_cohort_week_assignment.sql
+++ queries/04_cohort_week_assignment.sql
@@ -1,6 +1,5 @@
-SELECT
-  user_pseudo_id,
-  DATE_TRUNC(PARSE_DATE('%Y%m%d', MIN(event_date)), WEEK) AS cohort_week
+SELECT user_pseudo_id,
+DATE_TRUNC(PARSE_DATE('%Y%m%d', MIN(event_date)), WEEK) AS cohort_week
 FROM `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`
 GROUP BY user_pseudo_id
 LIMIT 20
