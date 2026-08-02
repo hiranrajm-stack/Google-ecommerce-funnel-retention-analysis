b24fb72f2588984900ace2b101cf8be321126b29
hiranrajm-stack <mhiranraj@gmail.com>
2026-08-02 12:10:13 +0530

    Update 05_user_weekly_activity.sql

--- queries/05_user_weekly_activity.sql
+++ queries/05_user_weekly_activity.sql
@@ -1,5 +1,4 @@
-SELECT
-  user_pseudo_id,
-  DATE_TRUNC(PARSE_DATE('%Y%m%d', event_date), WEEK) AS activity_week
+SELECT user_pseudo_id,
+DATE_TRUNC(PARSE_DATE('%Y%m%d', event_date), WEEK) AS activity_week
 FROM `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`
 GROUP BY user_pseudo_id, activity_week
